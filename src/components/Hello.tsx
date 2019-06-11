8f10a3b583ca40c717b473269d22bc97b2cbae23
nohtaesang <nohtaesang@naver.com>
2019-06-11 15:41:24 +0900

    .

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -7,6 +7,27 @@ export interface HelloProps {
 
 export class Hello extends React.Component<HelloProps, {}> {
 	render() {
+		let value = 1;
+
+		let obj = {
+			value: 100,
+			foo: function() {
+				let that = this; // Workaround : this === obj
+
+				console.log("foo's this: ", this); // obj
+				console.log("foo's this.value: ", this.value); // 100
+				function bar() {
+					console.log("bar's this: ", this); // window
+					console.log("bar's this.value: ", this.value); // 1
+					// console.log("bar's that: ", that); // obj
+					// console.log("bar's that.value: ", that.value); // 100
+				}
+				bar();
+			}
+		};
+
+		obj.foo();
+
 		return (
 			<h1>
 				Hello from {this.props.compiler} and {this.props.framework}!
